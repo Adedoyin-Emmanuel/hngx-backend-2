8484fdbd52feb7a6b54c2460e3f77b9dde5a59c7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-11 04:12:43 +0100

    Connected the person model with controller

--- controllers/person.controller.js
+++ controllers/person.controller.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 const response = require("./../utils/response");
-//person model
+const { Person } = require("./../models");
 
 class PersonController {
   static async create(req, res) {
@@ -12,7 +12,8 @@ class PersonController {
       const { error, value } = requestSchema.validate(req.body);
       if (error) return response(res, 400, error.details[0].message);
 
-      //store the person inside the database
+      await Person.create(value);
+      return response(res, 201, "Person created successfully");
     } catch (error) {
       console.log(`An error occured ${error}`);
       return response(res, 500, "Could not create person");
@@ -29,6 +30,15 @@ class PersonController {
       if (error) return response(res, 400, error.details[0].message);
 
       //get the person with that Id from the database
+      await Person.findById(value.id)
+        .exec()
+        .then((person) => {
+          return response(res, 200, person);
+        })
+        .catch((error) => {
+          console.log(`An error occured ${error}`);
+          return response(res, 404, "Person not found");
+        });
     } catch (error) {
       console.log(`An error occured ${error}`);
       return response(res, 500, "Could not get person");
@@ -38,7 +48,8 @@ class PersonController {
   static async getAll(req, res) {
     try {
       //get all persons from the database
-      return response(res, 200, {});
+      const allPersons = await Person.find();
+      return response(res, 200, allPersons);
     } catch (error) {
       console.log(`An error occured ${error}`);
       return response(res, 500, "Could not retrive persons from db");
@@ -56,6 +67,7 @@ class PersonController {
       if (error) return response(res, 400, error.details[0].message);
 
       //update the person value in the database
+      // await Person.findByIdAndUpdate(value.id)
     } catch (error) {
       console.log(`An error occured ${error}`);
       return response(res, 500, "Could not retrive persons from db");
@@ -72,6 +84,8 @@ class PersonController {
       if (error) return response(res, 400, error.details[0].message);
 
       //delete the person value in the database
+      await Person.findByIdAndDelete(value.id).exec();
+      return response(res, 200, "Person deleted successfully");
     } catch (error) {
       console.log(`An error occured ${error}`);
       return response(res, 500, "Could not delete person");
